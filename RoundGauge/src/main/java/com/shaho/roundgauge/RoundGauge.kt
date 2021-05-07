0fbafa63e59dfd3d75ccc54762b89d2054e77355
Sh.Bashoki <bashoki.shaho@gmail.com>
2021-05-07 21:05:25 +0430

    Set dynamic values

--- RoundGauge/src/main/java/com/shaho/roundgauge/RoundGauge.kt
+++ RoundGauge/src/main/java/com/shaho/roundgauge/RoundGauge.kt
@@ -2,65 +2,106 @@ package com.shaho.roundgauge
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.cos
 import kotlin.math.min
+import kotlin.math.sin
 
 
 class RoundGauge(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-    private var mAnimator: ValueAnimator = ValueAnimator()
-    private val mRectF: RectF = RectF()
-    private val mCenterPoint: Point = Point()
-    private val mDefaultSize = dipToPx(150f)
-    private val mArcPaint: Paint = Paint()
-    private val mBgArcPaint: Paint = Paint()
-    private val mProgressArcPaint: Paint = Paint()
+    private var progressBarAnimator: ValueAnimator = ValueAnimator()
 
-    private var mValue = 50f
-    private val mMaxValue = 50f
-    private var mPercent = 0f
+    private var progressBarValue = 100f
+    private val progressBarMaxValue = 100f
+    private var progressBarPercent = 0f
+
+    private val backgroundPaint: Paint = Paint()
+    private val progressBarPaint: Paint = Paint()
+    private val markerPaint: Paint = Paint()
+    private val backgroundRectF: RectF = RectF()
+
+    private var gaugeBackgroundColor: Int = Color.BLACK
+    private var gaugeBackgroundStroke: Float = 50F
+    private var gaugeProgressBarColor: Int = Color.GREEN
+    private var gaugeProgressBarStroke: Float = 25F
+    private var gaugeProgressAcceleration: Float = 0F
+    private var gaugeProgressDuration: Int = 3000
+    private var gaugeProgressBarConstantAcceleration: Boolean = true
+    private var gaugeNumberOfMarkers: Int = 8
+    private var gaugeMarkersStroke: Float = 15F
+    private var gaugeMarkersLength: Int = 50
+    private var gaugeMarkersColor: Int = Color.BLACK
+
+    private var defaultSize = dipToPx()
 
     init {
-        initAttrs(attrs)
+        attrs?.let { itAttrs ->
+            initAttrs(itAttrs)
+        }
         initPoint()
-        setValue(mValue)
+        setValue(progressBarValue)
     }
 
-    private fun initAttrs(attrs: AttributeSet?) {
-//        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RoundGauge)
-//
-//        typedArray.recycle()
+    private fun initAttrs(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RoundGauge)
+
+        try {
+            gaugeBackgroundColor = typedArray.getColor(R.styleable.RoundGauge_gaugeBackgroundColor, Color.BLACK)
+            gaugeBackgroundStroke = typedArray.getFloat(R.styleable.RoundGauge_gaugeBackgroundStroke, 50F)
+            gaugeProgressBarColor = typedArray.getColor(R.styleable.RoundGauge_gaugeProgressBarColor, Color.GREEN)
+            gaugeProgressBarStroke = typedArray.getFloat(R.styleable.RoundGauge_gaugeProgressBarStroke, 25F)
+            gaugeProgressAcceleration = typedArray.getFloat(R.styleable.RoundGauge_gaugeProgressAcceleration, 0F)
+            gaugeProgressDuration = typedArray.getInt(R.styleable.RoundGauge_gaugeProgressDuration, 3000)
+            gaugeProgressBarConstantAcceleration = typedArray.getBoolean(R.styleable.RoundGauge_gaugeProgressBarConstantAcceleration, true)
+            gaugeNumberOfMarkers = typedArray.getInt(R.styleable.RoundGauge_gaugeNumberOfMarkers, 8)
+            gaugeMarkersStroke = typedArray.getFloat(R.styleable.RoundGauge_gaugeMarkersStroke, 15F)
+            gaugeMarkersLength = typedArray.getInt(R.styleable.RoundGauge_gaugeMarkersLength, 50)
+            gaugeMarkersColor = typedArray.getColor(R.styleable.RoundGauge_gaugeMarkersColor, Color.BLACK)
+        } finally {
+            typedArray.recycle()
+        }
     }
 
     private fun initPoint() {
-        mArcPaint.isAntiAlias=true
-        mArcPaint.style = Paint.Style.STROKE
-        mArcPaint.strokeWidth = 25F
-        mArcPaint.strokeCap=Paint.Cap.ROUND
-        mArcPaint.color = Color.BLACK
+        backgroundPaint.isAntiAlias = true
+        backgroundPaint.style = Paint.Style.STROKE
+        backgroundPaint.strokeWidth = gaugeBackgroundStroke
+        backgroundPaint.strokeCap = Paint.Cap.ROUND
+        backgroundPaint.color = gaugeBackgroundColor
 
-        mProgressArcPaint.isAntiAlias=true
-        mProgressArcPaint.style = Paint.Style.STROKE
-        mProgressArcPaint.strokeWidth = 25F
-        mProgressArcPaint.strokeCap=Paint.Cap.ROUND
-        mProgressArcPaint.color = Color.GREEN
+        progressBarPaint.isAntiAlias = true
+        progressBarPaint.style = Paint.Style.STROKE
+        progressBarPaint.strokeWidth = gaugeProgressBarStroke
+        progressBarPaint.strokeCap = Paint.Cap.ROUND
+        progressBarPaint.color = gaugeProgressBarColor
 
-        mBgArcPaint.isAntiAlias=true
-        mBgArcPaint.style = Paint.Style.STROKE
-        mBgArcPaint.strokeWidth = 50F
-        mBgArcPaint.strokeCap=Paint.Cap.ROUND
-        mBgArcPaint.color = Color.BLACK
+        markerPaint.isAntiAlias = true
+        markerPaint.style = Paint.Style.STROKE
+        markerPaint.strokeWidth = gaugeMarkersStroke
+        markerPaint.strokeCap = Paint.Cap.ROUND
+        markerPaint.color = gaugeMarkersColor
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        setMeasuredDimension(measureView(widthMeasureSpec, mDefaultSize), measureView(heightMeasureSpec, mDefaultSize))
+        setMeasuredDimension(measureView(widthMeasureSpec, defaultSize.toInt()), measureView(heightMeasureSpec, defaultSize.toInt()))
     }
 
-//    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-//        super.onSizeChanged(w, h, oldw, oldh)
-//    }
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        defaultSize = if (w < h) {
+            w.toFloat()
+        } else {
+            h.toFloat()
+        }
+
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -80,23 +121,35 @@ class RoundGauge(context: Context?, attrs: AttributeSet?) : View(context, attrs)
     private fun drawArc(canvas: Canvas?) {
         canvas?.save()
 
-        val rectf = RectF(50F, 50F, 600F, 600F)
-        canvas?.drawArc(rectf, 120F, 300F, false, mBgArcPaint)
+        val marginStroke = gaugeBackgroundStroke / 2
+        val insideMarkerLineRadius = (defaultSize / 2) - (marginStroke * 2)
+        val externalMarkerLineRadius = (defaultSize / 2) - gaugeMarkersLength - marginStroke - (gaugeMarkersStroke / 2)
+        val markerLineRadius = (defaultSize / 2)
 
-        for (i in 0 until 8) {
-            if ((45*i) > 150 && (45*i)<210)
+        backgroundRectF.left = marginStroke
+        backgroundRectF.top = marginStroke
+        backgroundRectF.right = defaultSize - marginStroke
+        backgroundRectF.bottom = defaultSize - marginStroke
+
+        canvas?.drawArc(backgroundRectF, 120F, 300F, false, backgroundPaint)
+
+        val partOfDegrees = 360.0 / gaugeNumberOfMarkers
+        for (i in 0 until gaugeNumberOfMarkers) {
+            val degrees: Double = i * partOfDegrees
+            if (degrees > 150 && degrees < 210)
                 continue
 
-            val startX: Float = 325F + Math.sin(Math.toRadians(45*i.toDouble())).toFloat() * (300 - 50)
-            val startY: Float = 325F - Math.cos(Math.toRadians(45*i.toDouble())).toFloat() * (300 - 50)
+            val radians: Double = Math.toRadians(degrees)
 
-            val stopX: Float = 325F + Math.sin(Math.toRadians(45*i.toDouble())).toFloat() * (300 - 100)
-            val stopY: Float = 325F - Math.cos(Math.toRadians(45*i.toDouble())).toFloat() * (300 - 100)
-            canvas?.drawLine(startX, startY, stopX, stopY, mArcPaint)
+            val startX: Float = markerLineRadius + sin(radians).toFloat() * insideMarkerLineRadius
+            val startY: Float = markerLineRadius - cos(radians).toFloat() * insideMarkerLineRadius
+
+            val stopX: Float = markerLineRadius + sin(radians).toFloat() * externalMarkerLineRadius
+            val stopY: Float = markerLineRadius - cos(radians).toFloat() * externalMarkerLineRadius
+            canvas?.drawLine(startX, startY, stopX, stopY, markerPaint)
         }
 
-
-        canvas?.drawArc(rectf, 120F, 300F*mPercent, false, mProgressArcPaint)
+        canvas?.drawArc(backgroundRectF, 120F, 300F * progressBarPercent, false, progressBarPaint)
 
         canvas?.restore()
     }
@@ -115,30 +168,30 @@ class RoundGauge(context: Context?, attrs: AttributeSet?) : View(context, attrs)
         return result
     }
 
-    private fun dipToPx(dip: Float): Int {
+    private fun dipToPx(dip: Float = 400F): Float {
         val density = resources.displayMetrics.density
-        return ((dip * density) + (0.5f * (if (dip >= 0) 1 else -1))).toInt()
+        return (dip * density) + (0.5f * (if (dip >= 0) 1 else -1))
     }
 
     fun setValue(value: Float) {
-        var value = value
-        if (value > mMaxValue) {
-            value = mMaxValue
+        var newValue = value
+        if (newValue > progressBarMaxValue) {
+            newValue = progressBarMaxValue
         }
-        val start: Float = mPercent
-        val end: Float = value / mMaxValue
-        startAnimator(start, end, 5000)
+        val start: Float = progressBarPercent
+        val end: Float = newValue / progressBarMaxValue
+        startAnimator(start, end)
     }
 
-    private fun startAnimator(start: Float, end: Float, animTime: Long) {
-        mAnimator = ValueAnimator.ofFloat(start, end)
-        mAnimator.duration = animTime
-        mAnimator.addUpdateListener { animation ->
-            mPercent = animation.animatedValue as Float
-            mValue = mPercent * mMaxValue
+    private fun startAnimator(start: Float, end: Float, animTime: Long = gaugeProgressDuration.toLong()) {
+        progressBarAnimator = ValueAnimator.ofFloat(start, end)
+        progressBarAnimator.duration = animTime
+        progressBarAnimator.addUpdateListener { animation ->
+            progressBarPercent = animation.animatedValue as Float
+            progressBarValue = progressBarPercent * progressBarMaxValue
 
             invalidate()
         }
-        mAnimator.start()
+        progressBarAnimator.start()
     }
 }
